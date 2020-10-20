54ed49d0835419fae273bb33cdc2877276fdc456
pranjal <test@test.com>
2020-10-20 23:32:56 +0530

    first commit

---
+++ build/precache-manifest.a07841f833eb0d58cabb6f6800ccbf6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2d9b3a35caf716aec0ef39c17d120f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd7c1e4f5b776b740f59",
+    "url": "/static/css/main.4bd01614.chunk.css"
+  },
+  {
+    "revision": "0c9d1d6a38ad5f308063",
+    "url": "/static/js/2.9a63fbb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a63fbb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd7c1e4f5b776b740f59",
+    "url": "/static/js/main.0f32e36d.chunk.js"
+  },
+  {
+    "revision": "9f94abe8ee74d818807a",
+    "url": "/static/js/runtime-main.cfbeaeda.js"
+  }
+]);
